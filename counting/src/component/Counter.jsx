adc5d6c79b2aa6ff7e3bba985f1befa5d6656ed0
dpansumisra <dpansumisra@gmail.com>
2024-10-06 00:24:17 +0530

    Add

---
+++ counting/src/component/Counter.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Counter = () => {
+  return (
+    <div className='counter-container'>
+      <p id='para'>You have Clicked 0 Times</p>
+        <button id='btn'>Click Me</button>
+    </div>
+  )
+}
+
+export default Counter
